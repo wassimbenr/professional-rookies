31ad788bd64304c2bfe18dff62b3de6fa9c4856b
dhiaabdelli1 <dhia.abdelli1@esprit.tn>
2020-04-03 23:56:49 +0100

    var changes

--- dhia/hero.c
+++ dhia/hero.c
@@ -13,8 +13,8 @@ void initialiser_hero(hero *h)
 	h->sprite.frame.x=0;
 	h->sprite.frame.y=0;
 
-	h->sprite.frame.w=80;
-	h->sprite.frame.h=72;
+	h->sprite.frame.w=h->sprite.image->w/5;
+	h->sprite.frame.h=h->sprite.image->h/7;
 	h->sprite.curframe=0;
 
 
@@ -97,38 +97,38 @@ void animer_hero(hero *h,movement movement)
 			h->sprite.maxframe=3;
 			break;
 		case WALK:
-			h->sprite.frame.y=72;
+			h->sprite.frame.y=h->sprite.frame.h;
 			h->sprite.maxframe=4;
 			h->movement=WALK;
 			break;
 		case JUMP:
-			h->sprite.frame.y=144;
+			h->sprite.frame.y=2*h->sprite.frame.h;
 			h->sprite.maxframe=2;
 			h->movement=JUMP;
 			break;
 		case PUNCH:
-			h->sprite.frame.y=216;
+			h->sprite.frame.y=3*h->sprite.frame.h;
 			h->sprite.maxframe=3;
 			h->movement=PUNCH;
 			break;
 		case KICK:
-			h->sprite.frame.y=288;
+			h->sprite.frame.y=4*h->sprite.frame.h;
 			h->sprite.maxframe=3;
 			h->movement=KICK;
 			break;
 		case HIT:
-			h->sprite.frame.y=360;
+			h->sprite.frame.y=5*h->sprite.frame.h;
 			h->sprite.maxframe=4;
 			h->movement=HIT;
 			break;
 		case DIE:
-			h->sprite.frame.y=432;
+			h->sprite.frame.y=6*h->sprite.frame.h;
 			h->sprite.maxframe=2;
 			h->movement=DIE;
 			break;
 	}
 	tempsActuel=SDL_GetTicks();
-	if (tempsActuel-tempsPrecedent >150)
+	if (tempsActuel-tempsPrecedent >200)
 	{
 		if (h->sprite.curframe >= h->sprite.maxframe)
 		{		
